48482a72dbe6681e840b9d7ef03e29a2ba8f4ea5
Haozhe Xie <root@haozhexie.com>
2025-06-25 19:01:55 +0800

    feat: allow append to existing LeRobot datasets.

--- scripts/create_lerobot_dataset.py
+++ scripts/create_lerobot_dataset.py
@@ -4,7 +4,7 @@
 # @Author: Haozhe Xie
 # @Date:   2025-05-30 10:43:57
 # @Last Modified by: Haozhe Xie
-# @Last Modified at: 2025-06-17 16:30:11
+# @Last Modified at: 2025-06-25 19:00:06
 # @Email:  root@haozhexie.com
 #
 # Ref: https://github.com/Physical-Intelligence/openpi/blob/main/examples/libero/convert_libero_data_to_lerobot.py
@@ -22,6 +22,7 @@ import huggingface_hub.constants
 import lerobot.common.datasets.lerobot_dataset
 import lerobot.common.datasets.utils
 import numpy as np
+import scipy.spatial.transform
 from tqdm import tqdm
 
 PROJECT_HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
@@ -66,50 +67,53 @@ def get_episode_metadata(episode_path):
     }
 
 
-def create_lerobot_dataset(repo_id, metadata):
+def _get_fields(prefixes, rot_fmt="quat"):
+    fields = []
+    for p in prefixes:
+        if p.find("_pos_") != -1:
+            fields.append(p + "x")
+            fields.append(p + "y")
+            fields.append(p + "z")
+        elif p.find("_rot_") != -1 and rot_fmt == "quat":
+            fields.append(p + "qw")
+            fields.append(p + "qx")
+            fields.append(p + "qy")
+            fields.append(p + "qz")
+        elif p.find("_rot_") != -1 and rot_fmt == "euler":
+            fields.append(p + "x")
+            fields.append(p + "y")
+            fields.append(p + "z")
+        elif p.find("_vel_") != -1:
+            fields.append(p + "x")
+            fields.append(p + "y")
+            fields.append(p + "z")
+        else:
+            fields.append(p)
+
+    return fields
+
+
+def create_lerobot_dataset(repo_id, metadata, rot_fmt="quat"):
+    action_fields = _get_fields(["ee_pos_", "ee_rot_", "gripper"], rot_fmt)
+    state_fields = _get_fields(["ee_pos_", "ee_rot_"], rot_fmt)
+    env_state_fields = _get_fields(
+        ["object_pos_", "object_rot_", "object_vel_"], rot_fmt
+    )
     features = {
         "action": {
             "dtype": "float32",
-            "shape": (8,),
-            "names": [
-                "ee_pos_x",
-                "ee_pos_y",
-                "ee_pos_z",
-                "ee_quat_w",
-                "ee_quat_x",
-                "ee_quat_y",
-                "ee_quat_z",
-                "gripper",
-            ],
+            "shape": (len(action_fields),),
+            "names": action_fields,
         },
         "observation.state": {
             "dtype": "float32",
-            "shape": (7,),
-            "names": [
-                "ee_pos_x",
-                "ee_pos_y",
-                "ee_pos_z",
-                "ee_quat_w",
-                "ee_quat_x",
-                "ee_quat_y",
-                "ee_quat_z",
-            ],
+            "shape": (len(state_fields),),
+            "names": state_fields,
         },
         "observation.environment_state": {
             "dtype": "float32",
-            "shape": (10,),
-            "names": [
-                "object_pos_x",
-                "object_pos_y",
-                "object_pos_z",
-                "object_quat_qw",
-                "object_quat_qx",
-                "object_quat_qy",
-                "object_quat_qz",
-                "object_vel_x",
-                "object_vel_y",
-                "object_vel_z",
-            ],
+            "shape": (len(env_state_fields),),
+            "names": env_state_fields,
         },
     }
     for c in metadata["cameras"]:
@@ -118,7 +122,7 @@ def create_lerobot_dataset(repo_id, metadata):
         for m in ["rgb"]:
             # Shorten the name for semantic segmentation
             m = "seg" if m == "semantic_segmentation" else m
-            features["observation.images.%s_%s" % (c["name"], m)] = {
+            features["observation.image"] = {
                 "dtype": "video",
                 "names": ["height", "width", "channel"],
                 "shape": (c["height"], c["width"], 3 if m == "rgb" else 1),
@@ -137,7 +141,7 @@ def create_lerobot_dataset(repo_id, metadata):
         fps=metadata["fps"],
         features=features,
         image_writer_threads=32,
-        image_writer_processes=8,
+        image_writer_processes=32,
     )
 
 
@@ -147,71 +151,126 @@ def get_task_instruction(episode_file_name):
     )
 
 
-def get_episode_frames(episode_path):
+def _get_rotation_vector(quat, format="quat"):
+    if format == "quat":
+        return quat
+    elif format == "euler":
+        return scipy.spatial.transform.Rotation.from_quat(quat).as_euler(
+            "xyz", degrees=False
+        )
+    else:
+        raise ValueError("Unsupported format: %s. Use 'quat' or 'euler'." % format)
+
+
+def get_episode_frames(episode_path, rot_fmt="quat"):
     with h5py.File(episode_path, "r") as f:
         env_states = {k: f[k][()] for k in f.keys()}
 
     frames = []
     for i in range(len(env_states["action"])):
         _frame = {
-            "action": env_states["action"][i],
+            "action": np.concatenate(
+                [
+                    env_states["action"][i][:3],
+                    _get_rotation_vector(env_states["action"][i][3:-1], rot_fmt),
+                    env_states["action"][i][-1:],
+                ],
+                axis=-1,
+            ).astype(np.float32),
             "observation.state": np.concatenate(
                 [
                     env_states["ee_pos"][i],
-                    env_states["ee_quat"][i],
+                    _get_rotation_vector(env_states["ee_quat"][i], rot_fmt),
                 ],
                 axis=-1,
-            ),
+            ).astype(np.float32),
             "observation.environment_state": np.concatenate(
                 [
                     env_states["object_pos"][i],
-                    env_states["object_quat"][i],
+                    _get_rotation_vector(env_states["object_quat"][i], rot_fmt),
                     env_states["object_vel"][i],
                 ],
                 axis=-1,
-            ),
+            ).astype(np.float32),
         }
         # Add camera frames
         for k, v in env_states.items():
             # if k.find("_cam_") == -1:
             # TODO: Only RGB is supported in LeRobotDataset (wait for upstream support)
-            if k.find("_cam_rgb") == -1:
+            if k.find("side_cam_rgb") == -1:
                 continue
-            _frame["observation.images.%s" % k] = v[i]
+            _frame["observation.image"] = v[i]
 
         frames.append(_frame)
 
     return frames
 
 
-def main(input_dir, push_to_hub):
+# def
+
+
+def main(input_dir, rot_fmt, push_to_hub):
     REPO_ID = "hzxie/dynamic_objects"
     OUTPUT_DIR = os.path.join(huggingface_hub.constants.HF_HOME, "lerobot", REPO_ID)
-
-    logging.info("Creating LeRobot dataset from %s to %s" % (input_dir, OUTPUT_DIR))
-    if os.path.exists(OUTPUT_DIR):
-        logging.warning(
-            "Output directory %s already exists. It will be overwritten." % OUTPUT_DIR
-        )
-        shutil.rmtree(OUTPUT_DIR)
-
-    episodes = [f for f in os.listdir(input_dir) if f.endswith(".h5")]
+    # Listing all episodes in the input directory
+    episodes = sorted([f for f in os.listdir(input_dir) if f.endswith(".h5")])
     if not episodes:
         logging.error("No episodes found in the input directory: %s" % input_dir)
         sys.exit(2)
 
-    dataset_metadata = get_episode_metadata(os.path.join(input_dir, episodes[0]))
-    logging.info("Episode metadata: %s" % dataset_metadata)
-    lerobot_dataset = create_lerobot_dataset(REPO_ID, dataset_metadata)
-    logging.info("Dataset Overview: %s" % lerobot_dataset)
+    # Check whether the output directory exists
+    overwrite = True
+    existing_episodes = []
+    if os.path.exists(OUTPUT_DIR):
+        answer = (
+            input(
+                "Output directory %s already exists. Do you want to overwrite? (y/N) "
+                % OUTPUT_DIR
+            )
+            .strip()
+            .lower()
+        )
+        if answer in ["y", "yes"]:
+            shutil.rmtree(OUTPUT_DIR)
+        else:
+            overwrite = False
+            existing_episodes = [
+                ep["filename"]
+                for ep in lerobot.common.datasets.utils.load_jsonlines(
+                    pathlib.Path(os.path.join(OUTPUT_DIR, "meta", "camera.jsonl"))
+                )
+            ]
 
-    for i, e in enumerate(tqdm(episodes)):
-        if e.find(dataset_metadata["robot_type"]) == -1:
+    # Creating the dataset in LeRobot format
+    episode_metadata = get_episode_metadata(os.path.join(input_dir, episodes[0]))
+    logging.info("Episode metadata: %s" % episode_metadata)
+    if overwrite:
+        logging.info("Creating LeRobot dataset from %s to %s" % (input_dir, OUTPUT_DIR))
+        lerobot_dataset = create_lerobot_dataset(REPO_ID, episode_metadata, rot_fmt)
+        logging.info("Dataset Overview: %s" % lerobot_dataset)
+    else:
+        lerobot_dataset = lerobot.common.datasets.lerobot_dataset.LeRobotDataset(
+            repo_id=REPO_ID,
+            root=OUTPUT_DIR,
+        )
+
+    ep_idx = len(existing_episodes)
+    for e in tqdm(episodes):
+        if e.find(episode_metadata["robot_type"]) == -1:
+            logging.warning(
+                "Skipping episode %s as it does not match the robot type %s"
+                % (e, episode_metadata["robot_type"])
+            )
+            continue
+        if e in existing_episodes:
+            logging.warning(
+                "Skipping episode %s as it already exists in the dataset" % e
+            )
             continue
 
         try:
             _metadata = get_episode_metadata(os.path.join(input_dir, e))
-            _frames = get_episode_frames(os.path.join(input_dir, e))
+            _frames = get_episode_frames(os.path.join(input_dir, e), rot_fmt)
             _task = get_task_instruction(os.path.basename(e))
             for f in _frames:
                 lerobot_dataset.add_frame(f, _task)
@@ -222,13 +281,14 @@ def main(input_dir, push_to_hub):
         lerobot_dataset.save_episode()
         # Manually save the camera parameters
         lerobot.common.datasets.utils.append_jsonlines(
-            {"episode_idx": i, "filename": e, "cameras": _metadata["cameras"]},
+            {"episode_idx": ep_idx, "filename": e, "cameras": _metadata["cameras"]},
             pathlib.Path(os.path.join(OUTPUT_DIR, "meta", "camera.jsonl")),
         )
+        ep_idx += 1
 
     if push_to_hub:
         lerobot_dataset.push_to_hub(
-            tags=["LeRobot", dataset_metadata["robot_type"], "synthetic", "dynamic"],
+            tags=["LeRobot", episode_metadata["robot_type"], "synthetic", "dynamic"],
             private=False,
             push_videos=True,
             license="apache-2.0",
@@ -240,7 +300,6 @@ if __name__ == "__main__":
         level=logging.INFO,
         format="[%(levelname)s] %(asctime)s %(message)s",
     )
-
     parser = argparse.ArgumentParser(description="Create LeRobot dataset")
     parser.add_argument(
         "--dataset_dir",
@@ -248,9 +307,14 @@ if __name__ == "__main__":
         required=True,
     )
     parser.add_argument(
+        "--rotation",
+        type=str,
+        default="quat",
+    )
+    parser.add_argument(
         "--push_to_hub",
         action="store_true",
     )
     args = parser.parse_args()
 
-    main(args.dataset_dir, args.push_to_hub)
+    main(args.dataset_dir, args.rotation, args.push_to_hub)
