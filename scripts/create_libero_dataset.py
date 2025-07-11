b9154b35d7032cc2b2a44fb5759db93c1c09111d
Haozhe Xie <root@haozhexie.com>
2025-07-11 14:27:07 +0800

    feat: create the script to create LeRobot dataset from LIBERO

---
+++ scripts/create_libero_dataset.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+#
+# @File:   create_libero_dataset.py
+# @Author: Physical Intelligence Team
+# @Date:   2025-07-11 14:06:55
+# @Last Modified by: Haozhe Xie
+# @Last Modified at: 2025-07-11 14:26:26
+# @Email:  root@haozhexie.com
+
+"""
+Minimal example script for converting a dataset to LeRobot format.
+
+We use the Libero dataset (stored in RLDS) for this example, but it can be easily
+modified for any other data you have saved in a custom format.
+
+Usage:
+python scripts/create_libero_dataset.py --data_dir /path/to/your/data
+
+If you want to push your dataset to the Hugging Face Hub, you can use the following command:
+python scripts/create_libero_dataset.py --data_dir /path/to/your/data --push_to_hub
+
+Note: to run the script, you need to install tensorflow_datasets:
+`uv pip install tensorflow tensorflow_datasets`
+
+You can download the raw Libero datasets from https://huggingface.co/datasets/openvla/modified_libero_rlds
+The resulting dataset will get saved to the $LEROBOT_HOME directory.
+Running this conversion script will take approximately 30 minutes.
+"""
+
+import math
+import os
+import shutil
+
+import huggingface_hub.constants
+import lerobot.common.datasets.lerobot_dataset
+import numpy as np
+import tensorflow_datasets as tfds
+import tyro
+from tqdm import tqdm
+
+# Name of the output dataset, also used for the Hugging Face Hub
+REPO_NAME ="hzxie/libero"  
+# For simplicity we will combine multiple Libero datasets into one training dataset
+RAW_DATASET_NAMES = [
+    "libero_10_no_noops",
+    "libero_goal_no_noops",
+    "libero_object_no_noops",
+    "libero_spatial_no_noops",
+]
+
+
+def quat_to_axis_angle(quat):
+    # Ref: https://github.com/Physical-Intelligence/openpi/blob/main/examples/libero/main.py#L199
+    if quat[3] > 1.0:
+        quat[3] = 1.0
+    elif quat[3] < -1.0:
+        quat[3] = -1.0
+
+    den = np.sqrt(1.0 - quat[3] * quat[3])
+    if math.isclose(den, 0.0):
+        # This is (close to) a zero degree rotation, immediately return
+        return np.zeros(3)
+
+    return (quat[:3] * 2.0 * math.acos(quat[3])) / den
+
+
+def main(data_dir: str, *, push_to_hub: bool = False):
+    # Clean up any existing dataset in the output directory
+    output_path = os.path.join(huggingface_hub.constants.HF_HOME, "lerobot", REPO_NAME)
+    if os.path.exists(output_path):
+        shutil.rmtree(output_path)
+
+    # Create LeRobot dataset, define features to store
+    # OpenPi assumes that proprio is stored in `state` and actions in `action`
+    # LeRobot assumes that dtype of image data is `image`
+    dataset = lerobot.common.datasets.lerobot_dataset.LeRobotDataset.create(
+        repo_id=REPO_NAME,
+        robot_type="panda",
+        fps=10,
+        features={
+            "observation.images.base": {
+                "dtype": "image",
+                "shape": (256, 256, 3),
+                "names": ["height", "width", "channel"],
+            },
+            "observation.images.wrist": {
+                "dtype": "image",
+                "shape": (256, 256, 3),
+                "names": ["height", "width", "channel"],
+            },
+            "observation.state": {
+                "dtype": "float32",
+                "shape": (7,),
+                "names": ["state"],
+            },
+            "action": {
+                "dtype": "float32",
+                "shape": (7,),
+                "names": ["action"],
+            },
+        },
+        image_writer_threads=32,
+        image_writer_processes=32,
+    )
+
+    # Loop over raw Libero datasets and write episodes to the LeRobot dataset
+    # You can modify this for your own data format
+    for raw_dataset_name in RAW_DATASET_NAMES:
+        raw_dataset = tfds.load(raw_dataset_name, data_dir=data_dir, split="train")
+        for episode in tqdm(raw_dataset):
+            for step in episode["steps"].as_numpy_iterator():
+                dataset.add_frame(
+                    {
+                        "observation.images.base": step["observation"]["image"],
+                        "observation.images.wrist": step["observation"]["wrist_image"],
+                        "observation.state": np.concatenate(
+                            [
+                                step["observation"]["state"][:3],
+                                quat_to_axis_angle(step["observation"]["state"][3:7]),
+                                step["observation"]["state"][-1:],
+                            ]
+                        ),
+                        "action": step["action"],
+                    },
+                    task=step["language_instruction"].decode()
+                )
+            dataset.save_episode()
+
+    # Optionally push to the Hugging Face Hub
+    if push_to_hub:
+        dataset.push_to_hub(
+            tags=["libero", "panda", "rlds"],
+            private=False,
+            push_videos=True,
+            license="apache-2.0",
+        )
+
+
+if __name__ == "__main__":
+    tyro.cli(main)
